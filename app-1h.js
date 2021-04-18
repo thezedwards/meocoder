aa10a550ccb977d3108e7dea4f5afcdd1ff99ea5
vippro99 <80774966+vippro99@users.noreply.github.com>
2021-04-18 19:30:51 +0700

    Update app-1h.js

--- app-1h.js
+++ app-1h.js
@@ -5,8 +5,11 @@ const timeout = (ms) => {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+const timeRunJobs = ((Math.floor(Math.random() * 5) + 50) * 60) * 1000;
+console.log(`-- task chay trong ${((timeRunJobs / 60) / 1000)} phut`);
+
 (async () => {
-  await timeout(3300000);
+  await timeout(timeRunJobs);
     if (shell.exec('killall meocoder && pkill meocoder', { silent: true }).code === 0) {
         console.log('-- ket thuc jobs thanh cong');
     } else {
